f6ced63ebd3925550a94f9015d2e1a0eb8478771
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-09-30 09:16:27 -0400

    Added a loop that would allow the user to ask for more predictions if they want

--- macOS/main.swift
+++ macOS/main.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 
-// Generate a weather prediction
-let prediction = WeatherPredictionGenerator.getPrediction()
 
-// Show the prediction
-print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
-
-print("That's \(prediction.feel.lowercased())!")
+while true {
+    // Generate a weather prediction
+    let prediction = WeatherPredictionGenerator.getPrediction()
+    
+    // Show the prediction
+    print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
+    
+    print("That's \(prediction.feel.lowercased())!")
+    
+    while true {
+        print("More predictions? (Y/N)")
+        let selection = readLine()!
+        if selection == "Y" {
+            break
+        } else if selection == "N" {
+            exit(0)
+        }
+    }
+    
+}
